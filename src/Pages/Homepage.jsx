105897342dfe9050056a532ba668d7c27587f0aa
Richmon Jay Francisco <jayjo.frncsc@gmail.com>
2025-11-26 21:31:10 +0800

    with motion framer

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -1,12 +1,14 @@
 import '../PagesCss/Homepage.css'
 import { Navigation } from '../Components/Navigation.jsx'
 import { Home } from '../Components/Home.jsx'
+import {Education} from '../Components/Education.jsx'
  
 function Homepage(){
     return(
         <div className='h-screen '>
             <Navigation/>
             <Home />
+            <Education />
             
         </div>
     )
